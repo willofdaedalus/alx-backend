940d5a9a7ce90e309b6d1eca674919afcc3085ac
Daedalus <manny.is.daedalus@pm.me>
2024-04-04 01:55:44 +0000

    seperated class from main files

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,39 +1,10 @@
 #!/usr/bin/env python3
 """ BaseCaching module """
 
-from functools import cache
+caching = __import__('base_caching')
 
 
-class BaseCaching():
-    """
-        BaseCaching defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        """ Initiliaze """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
-    def put(self, key, item):
-        """ Add an item in the cache """
-        raise NotImplementedError(
-                "put must be implemented in your cache class")
-
-    def get(self, key):
-        """ Get an item by key """
-        raise NotImplementedError(
-                "get must be implemented in your cache class")
-
-
-class FIFOCache(BaseCaching):
+class FIFOCache(caching.BaseCaching):
     """ first in first out caching system """
 
     def __init__(self):

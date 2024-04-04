8e464b91b8c4540b177bcbef3fc487583baa893f
Daedalus <manny.is.daedalus@pm.me>
2024-04-04 05:57:36 +0000

    task 3 and 4

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,43 +1,14 @@
 #!/usr/bin/env python3
 """ BaseCaching module """
 
-
-from collections import OrderedDict
-
-
-class BaseCaching():
-    """
-        BaseCaching defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        """ Initiliaze """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
-    def put(self, key, item):
-        """ Add an item in the cache """
-        raise NotImplementedError(
-                "put must be implemented in your cache class")
-
-    def get(self, key):
-        """ Get an item by key """
-        raise NotImplementedError(
-                "get must be implemented in your cache class")
+from datetime import datetime
+caching = __import__("base_caching")
 
 
-class LRUCache(BaseCaching):
+class LRUCache(caching.BaseCaching):
     """ last in first out caching system """
     # save the last accessed key for faster and easier removal
-    __ord_dict = OrderedDict()
+    __item_tracker = {}
 
     def __init__(self):
         """ initialize the class """
@@ -48,14 +19,17 @@ class LRUCache(BaseCaching):
         if key and item:
             if key not in self.cache_data:
                 if len(self.cache_data) >= self.MAX_ITEMS:
-                    to_rm = list(self.__ord_dict.keys())[-1]
-                    print(f"to remove is {to_rm}")
+                    to_rm = min(self.__item_tracker)
+                    self.cache_data.pop(to_rm)
+                    self.__item_tracker.pop(to_rm)
+                    print("DISCARD: {}".format(to_rm))
 
             self.cache_data[key] = item
-            self.__ord_dict[key] = item
+            self.__item_tracker[key] = datetime.now()
 
     def get(self, key):
         """ Get an item by key """
-        if not key:
+        if key not in self.cache_data:
             return None
+        self.__item_tracker[key] = datetime.now()
         return self.cache_data[key]
--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,40 +1,14 @@
 #!/usr/bin/env python3
 """ BaseCaching module """
 
-
-class BaseCaching():
-    """
-        BaseCaching defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        """ Initiliaze """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
-    def put(self, key, item):
-        """ Add an item in the cache """
-        raise NotImplementedError(
-                "put must be implemented in your cache class")
-
-    def get(self, key):
-        """ Get an item by key """
-        raise NotImplementedError(
-                "get must be implemented in your cache class")
+from datetime import datetime
+caching = __import__("base_caching")
 
 
-class MRUCache(BaseCaching):
+class MRUCache(caching.BaseCaching):
     """ last in first out caching system """
     # save the last accessed key for faster and easier removal
-    __last_key = None
+    __item_tracker = {}
 
     def __init__(self):
         """ initialize the class """
@@ -45,14 +19,17 @@ class MRUCache(BaseCaching):
         if key and item:
             if key not in self.cache_data:
                 if len(self.cache_data) >= self.MAX_ITEMS:
-                    self.cache_data.pop(self.__last_key)
-                    print("DISCARD: {}".format(self.__last_key))
+                    to_rm = max(self.__item_tracker)
+                    self.cache_data.pop(to_rm)
+                    self.__item_tracker.pop(to_rm)
+                    print("DISCARD: {}".format(to_rm))
 
             self.cache_data[key] = item
-            self.__last_key = key
+            self.__item_tracker[key] = datetime.now()
 
     def get(self, key):
         """ Get an item by key """
-        if not key:
+        if key not in self.cache_data:
             return None
+        self.__item_tracker[key] = datetime.now()
         return self.cache_data[key]
